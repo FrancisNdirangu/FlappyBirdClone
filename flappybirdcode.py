4e1287cf10e4e296d3162b8beadbbd5c6ef5d523
FrancisNdirangu <frankngugi@gmail.com>
2022-05-06 14:30:07 -0500

    created a black screen with quit functionality

--- flappybirdcode.py
+++ flappybirdcode.py
@@ -1,1 +1,14 @@
 #hello this is the file
+import pygame
+
+screen = pygame.display.set_mode((600,500))
+pygame.display.set_caption('FlappyBirdClone')
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
