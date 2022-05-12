cefe9337c123e62acedf4893279b50b175db553f
FrancisNdirangu <frankngugi@gmail.com>
2022-05-12 13:45:29 -0500

    still getting an error from trying to import stuff from settimes

--- flappybirdcode.py
+++ flappybirdcode.py
@@ -1,12 +1,16 @@
 #hello this is the file
-import pygame
+import pygame,sys,time
 from player import Player
-from random import choice
 from sprites import Background
+from settings import *
 
 
 class Game:
     def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        pygame.display.set_caption('Flappy_Bird')
+        self.clock = pygame.time.Clock()
         #player setup
         player_sprite = Player(45,60,5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
@@ -14,38 +18,29 @@ class Game:
         #sprites setup
         Background(self.all_sprites)
 
-        #score setup
-        self.score = 0
-
-
-        #obstacle setup
-        self.collision_sprites = pygame.sprite.group()
-
-        #all_sprites group 
+        #sprite groups
+        self.collision_sprites = pygame.sprite.group() 
         self.all_sprites = pygame.sprite.group()
 #in order to get consistent smooth movement use delta time. which Clear Code has a video on
 
-    def obstacle_creater(self):
-        pass
-
-    def create_multiple_obstacles(self):
-        pass
-
-    def collision_checker(self):
-        pass
-
-    def display_level(self):
-        pass
-
-    def display_score(self):
-        pass
-
-    def victory_message(self):
-        pass
-
     def run(self):
-        self.player.update()
-        self.player.draw(screen)
+
+
+        last_time = time.time()
+        while True:
+            #delta time
+            dt = time.time() -last_time
+            last_time = time.time()
+
+            #event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            #game logic
+            pygame.display.update()
+            self.clock.tick(FRAMERATE)
 
 
 
@@ -56,31 +51,5 @@ class Game:
 
 
 if __name__ == '__main__':
-    pygame.init()
-    screen_width = 600
-    screen_height = 600
-    screen = pygame.display.set_mode((screen_width,screen_height))
-    pygame.display.set_caption('FlappyBirdClone')
-  
-    
-    running = True
-    clock = pygame.time.Clock()
     game = Game()
-
-    while running:
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.fill([0,0,0])
-
-
-        
-        game.run() #when you make the run function in the game class
-
-
-
-        pygame.display.flip()
-        clock.tick(60)
+    game.run()
--- sprites.py
+++ sprites.py
@@ -1,10 +1,11 @@
 import pygame
+from settings import *
 
 
 
 
 class Background(pygame.sprite.Sprite):
     def __init__(self,groups):
-        super().__init__()
+        super().__init__(groups)
         self.image = pygame.image.load('C:/Users/franc/Downloads/FlappyBirdClone/flappy-bird-assets-master/sprites/background-day.png')
         self.rect = self.image.get_rect(topleft = (0,0))
