9b993c9261e2298102ec3d3849b3ee042e6e8bf7
FrancisNdirangu <frankngugi@gmail.com>
2022-05-14 08:41:32 -0500

    made the background scale in size however there is a small black space on the edge

--- sprites.py
+++ sprites.py
@@ -7,10 +7,10 @@ from settings import *
 class Background(pygame.sprite.Sprite):
     def __init__(self,groups,scale_factor):
         super().__init__(groups)
-        background_image = pygame.image.load('C:/Users/franc/Downloads/FlappyBirdClone/flappy-bird-assets-master/sprites/background-day.png').get_height()
+        background_image = pygame.image.load('C:/Users/franc/Downloads/FlappyBirdClone/flappy-bird-assets-master/sprites/background-day.png')
 
-        FULL_HEIGHT = background_image.get_height() #* scale_factor 
-        FULL_WIDTH = background_image.get_width() #* scale_factor
+        FULL_HEIGHT = background_image.get_height() * scale_factor 
+        FULL_WIDTH = background_image.get_width() * scale_factor
 
-        self.image = pygame.transform.scale(background_image,(FULL_WIDTH,FULL_HEIGHT))
+        self.image = pygame.transform.scale(background_image,(int(FULL_WIDTH),int(FULL_HEIGHT)))
         self.rect = self.image.get_rect(topleft = (0,0))
